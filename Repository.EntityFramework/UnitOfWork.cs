6f8bd59cda34c611e33298ee206687dee6968bb1
victorcobden <vhcobden@gmail.com>
2016-11-02 08:16:14 -0500

    Added Unit Of Work

---
+++ Repository.EntityFramework/UnitOfWork.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Data.Entity;
+using System.Threading.Tasks;
+
+namespace Repository.EntityFramework
+{
+    public class UnitOfWork : Repository, IRepository, IUnitOfWork, IDisposable
+    {
+        public UnitOfWork(DbContext context, bool autoDetectChangesEnabled = false,
+                                          bool proxyCreationEnabled = false) : base(context,autoDetectChangesEnabled, proxyCreationEnabled)
+        {
+            
+        }
+
+        protected override int TrySaveChanges()
+        {
+            return 0;
+        }
+        protected override async Task<int> TrySaveChangesAsync()
+        {
+            return await Task.Run(()=> 0);
+        }
+
+        int IUnitOfWork.Save()
+        {
+            int Result = 0;
+
+            try
+            {
+                Result = Context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return Result;
+        }
+        async Task<int> IUnitOfWork.SaveAsync()
+        {
+            int Result = 0;
+
+            try
+            {
+                Result = await Context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return Result;
+        }
+    }
+}
---
+++ Repository/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace Repository
+{
+    public interface IUnitOfWork : IRepository
+    {
+        int Save();
+        Task<int> SaveAsync();
+    }
+}
